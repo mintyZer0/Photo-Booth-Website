435527ea9315ca130bbc047ade7e969e009b4143
minty <eithanmathewmalonzo@gmail.com>
2025-07-05 16:48:32 +0800

    Working video on webcam, no capture feature yet

---
+++ index.js
@@ -0,0 +1,43 @@
+// Check if mediaDevices API exists
+
+if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
+    console.log('mediaDevices Exists');
+    
+}
+'use strict';
+
+const video = document.getElementById('video');
+const canvas = document.getElementById('canvas');
+const snap = document.getElementById('snap')
+const errorMsgElement = document.getElementById('spanErrorMsg');
+localStorage
+
+const constraints = {
+    video:{
+        width: 1280,
+        height: 720,
+    }
+};
+
+async function init(){
+    try{
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        console.log(stream)
+        handleSuccess(stream);
+    }
+    catch(e){
+        errorMsgElement.innerHTML = `navigator.getUserMedia.error:${e.toString()}`;
+    }
+}
+
+function handleSuccess(stream){
+    //window.stream = stream;
+    video.srcObject = stream;
+}
+//Load init
+init();
+// Draw Image
+var context = canvas.getContext('2d');
+snap.addEventListener("click", function(){
+    context.drawImage(video, 0, 0, 640, 480)
+});
