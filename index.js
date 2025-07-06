be4a433bcea2c9aa50adc6e1f415b03051c25d52
minty <eithanmathewmalonzo@gmail.com>
2025-07-06 23:55:56 +0800

    - Added SSL Certificate for https - Trying to adjust image cropping so the snapped image matches the video feed but still not there yet

--- index.js
+++ index.js
@@ -8,17 +8,26 @@ if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
 
 const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
+const canvasHD = document.getElementById('canvas-HD')
 const snap = document.getElementById('snap')
 const photoGrid = document.getElementById('photo-grid-style1') 
 const errorMsgElement = document.getElementById('spanErrorMsg');
 let countdownText = document.getElementById('timer-countdown')
 let imagesList = []
-let timerLength = 5000
+let timerLength = 1000
 
 const constraints = {
     video:{
-        width: 1280,
-        height: 720,
+        width: {
+            min: 1280,
+            ideal: 1920,
+            max: 3480
+        },
+        height: {
+            min: 720,
+            ideal: 1080,
+            max: 2160,
+        }
     }
 };
 
@@ -40,8 +49,7 @@ function handleSuccess(stream){
 //Load init
 init();
 
-// Draw Image
-var context = canvas.getContext('2d');
+
 snap.addEventListener("click", function(){
     startTimer(timerLength);
 });
@@ -64,15 +72,55 @@ function startTimer(timerLength){
 
 }
 
-function snapImage(){
-    // Draw the image to canvas
-    context.drawImage(video, 0, 0, 100, 100);
-    // Convert canvas to image
-    var dataURL = canvas.toDataURL('image/png');
-    var img = document.createElement('img');
-    img.src = dataURL
-    imagesList.push(img)
-    photoGrid.appendChild(img)
-    console.log(imagesList)
+// Function to get camera Dimensions
+function getCameraResolution(video){
+    return new Promise((resolve) => {
+        video.addEventListener('loadeddata', 
+        resolve({
+            width: video.videoWidth,
+            height: video.videoHeight
+            })
+        );
+    });
+};
+
+ function storeHDImages(width, height) {
+    const contextHD = canvasHD.getContext('2d');
+    contextHD.drawImage(video, 0, 0, width, height);
+    const dataURL = canvasHD.toDataURL('image/png');
+    imagesList.push(dataURL);
+}
+
+ async function snapImage(){
+    try {
+        // Get camera resolution
+        camera = await getCameraResolution(video)
+        // Set canvas sizes
+        canvas.width = 100;
+        canvas.height = 100;
+        canvasHD.width = camera.width;    
+        canvasHD.height = camera.height;
+        // Store the hd image in array
+        storeHDImages(canvasHD.width, canvasHD.height);
+        let context = canvas.getContext('2d');
+        context.drawImage(video, 
+            (video.offsetWidth - 100) / 2, (video.offsetHeight- 100) / 2,
+            300, 300,
+            0, 0,
+            100, 100 
+        );
+        // Convert canvas to image
+        let dataURL = canvas.toDataURL('image/png');
+        let img = document.createElement('img');
+        img.src = dataURL;
+        // img.style.width = "100px";
+        // img.style.height = "100px";
+        photoGrid.appendChild(img);
+        
+    } catch (error) {
+        console.log(error);   
+        throw error;
+    }
+
 }
 
