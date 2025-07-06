f3c2dec40951959b554362b1b104dbbf3c563b3c
minty <eithanmathewmalonzo@gmail.com>
2025-07-06 17:16:32 +0800

    Created timer for snapping pictures and a countdown before snap

--- index.js
+++ index.js
@@ -9,8 +9,11 @@ if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
 const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
 const snap = document.getElementById('snap')
+const photoGrid = document.getElementById('photo-grid-style1') 
 const errorMsgElement = document.getElementById('spanErrorMsg');
-var imagesList = []
+let countdownText = document.getElementById('timer-countdown')
+let imagesList = []
+let timerLength = 5000
 
 const constraints = {
     video:{
@@ -40,11 +43,36 @@ init();
 // Draw Image
 var context = canvas.getContext('2d');
 snap.addEventListener("click", function(){
-    context.drawImage(video, 0, 0, 640, 480);
+    startTimer(timerLength);
+});
+
+function startTimer(timerLength){
+    //Start the timer to snap image
+    let imageTimer = setTimeout(snapImage, timerLength);
+    countdownText.style.display = "flex";
+    // Convert timer to seconds and decrement
+    let seconds = timerLength/1000;
+    countdownText.innerHTML = seconds;
+    let countdownTextTimer = setInterval(() => {
+        countdownText.innerHTML--;
+        // Stop the countdown text
+        if (Number(countdownText.innerHTML) <= 0){
+            clearInterval(countdownTextTimer)
+            countdownText.style.display = "none";
+        };
+    }, 1000);
+
+}
+
+function snapImage(){
+    // Draw the image to canvas
+    context.drawImage(video, 0, 0, 100, 100);
+    // Convert canvas to image
     var dataURL = canvas.toDataURL('image/png');
     var img = document.createElement('img');
     img.src = dataURL
     imagesList.push(img)
+    photoGrid.appendChild(img)
     console.log(imagesList)
-});
+}
 
